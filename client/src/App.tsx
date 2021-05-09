0a3c281bcbd7533c796d9f267a6abc1c022126aa
Susanna Persson <zestwerk@gmail.com>
2021-05-09 23:27:20 +0200

    two players error msg done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,34 +5,62 @@ import Player from './Player'
 import 'bulma';
 import './App.css';
 import ResetButton from './ResetButton';
+import ErrorMsg from './ErrorMsg';
 
 function App(props: any) {
   const [names, setNames] = useState<Player[]>([]);
   const [play, setPlay] = useState(false);
+  const [nameerror, setError] = useState(false);
 
   const addUser = (newUserName: string) => {
     const newUser = new Player(newUserName);
     setNames([...names, newUser]);
   };
 
-
-
   if (!play) {
-    return (
-      <div className="App section">
-        <Login setter={addUser} />
-        <input className="button" type="button" value="Done" onClick={() => {
-          if (names.length < 2) return; // must be at least two players. 
-          setPlay(true); 
-        }} />
-        <h2 className="title is-5" >Players</h2>
-        <ul className="columns">
-          {names.map(item =>
-            (<li className="column" key={item.toString()}>{item.toString()}</li>)
-          )}
-        </ul>
-      </div>
-    );
+    if (!nameerror) {
+      return (
+        <div className="App section">
+          <Login setter={addUser} />
+            <input className="button" type="button" value="Done" onClick={() => {
+            // must be at least two players. 
+            if (names.length < 2) {
+              setError(true);
+            } else {
+              setPlay(true); 
+            }
+            }} />
+          <h2 className="title is-5" >Players</h2>
+          <ul className="columns">
+            {names.map(item =>
+              (<li className="column" key={item.toString()}>{item.toString()}</li>)
+            )}
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App section">
+          <Login setter={addUser} />
+            <input className="button" type="button" value="Done" onClick={() => {
+            // must be at least two players. 
+            if (names.length < 2) {
+              setError(true);
+            } else {
+              setPlay(true); 
+            }
+            }} />
+          <ErrorMsg message='There needs to be at least two players to start the game!'></ErrorMsg>
+          <h2 className="title is-5" >Players</h2>
+          <ul className="columns">
+            {names.map(item =>
+              (<li className="column" key={item.toString()}>{item.toString()}</li>)
+            )}
+          </ul>
+        </div>
+      );
+    }
+ 
   } else {
     return (
       <div className="App section">
@@ -40,10 +68,8 @@ function App(props: any) {
         <Game players={names} />
         <ResetButton />
       </div>
-
     )
   }
-
 }
 
 export default App;
