d2829087f1151b6937ed1ee2d29da3fb62f1bafe
Delfi Sehidic <delfi.sehidic@gmail.com>
2021-05-14 13:41:43 +0200

    Move ResetButton from main page to Settings Menu

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -163,7 +163,7 @@ function App() {
       <div className="App section" onClick={() => navbarOpen ? setNavbarOpen(false) : undefined}>
         <h1 className="title is-3">Let's play!</h1>
         <Game activeGames={gameModuleSettings} gameService={gameService} players={players} gameModuleSerivce={gameModuleSerivce} />
-        <ResetButton />
+        
       </div>
       </div>
     )
