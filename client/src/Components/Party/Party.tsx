e3620b6a9ac5c071e21d541c6f41a117c11802db
Delfi Sehidic <delfi.sehidic@gmail.com>
2021-05-16 11:22:32 +0200

    Make "who won" buttons not stick to each other

--- client/src/Components/Party/Party.tsx
+++ client/src/Components/Party/Party.tsx
@@ -21,18 +21,25 @@ const Party: React.FC<TextGameModuleProps> = ({ gameService, gameEvent }) => {
             <div className="card-content">
                 <div data-testid="game-event" className="content">{addPlayerToGameEvent()}</div>
                 <h3>Who won?</h3>
+                <div className="columns">
+                    <div className="column">
+                        <button className="button" onClick={() => {
+                            gameService.addScore(players[1], 1)
+                            gameService.makeWinnerAlert(players[1])
+                            gameService.chooseRandomNewGame()
+                        }}>{players[1].toString()}</button>
+                    </div>
+                    <div className="column">
+                        <button className="button" onClick={() => {
+                            gameService.addScore(players[0], 1)
+                            gameService.makeWinnerAlert(players[0])
+                            gameService.chooseRandomNewGame()
+                        }}>{players[0].toString()}</button>
+                    </div>
+                </div>
 
-                <button className="button" onClick={() => {
-                    gameService.addScore(players[0], 1)
-                    gameService.makeWinnerAlert(players[0])
-                    gameService.chooseRandomNewGame()
-                }}>{players[0].toString()}</button>
 
-                <button className="button" onClick={() => {
-                    gameService.addScore(players[1], 1)
-                    gameService.makeWinnerAlert(players[1])
-                    gameService.chooseRandomNewGame()
-                }}>{players[1].toString()}</button>
+
 
             </div>
         </div>
