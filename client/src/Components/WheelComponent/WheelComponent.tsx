072ba3bb315792380b8c117960c03c52d1a3eff5
Caesar Lennartsson <cl223mf@student.lnu.se>
2021-05-19 16:15:34 +0200

    Lower wheel-size due to mobile responsive

--- client/src/Components/WheelComponent/WheelComponent.tsx
+++ client/src/Components/WheelComponent/WheelComponent.tsx
@@ -150,7 +150,7 @@ const WheelComponent: React.FC<AnimationGameModuleProps> = ({ gameService, curre
           const degree = (index * rotateDeg) //- 45
           
           return (
-            <div key={index} style={{transform: `rotate(${degree}deg)`, borderRight: `200px solid ${colors[index]}`}} className="arrow">
+            <div key={index} style={{transform: `rotate(${degree}deg)`, borderRight: `150px solid ${colors[index]}`}} className="arrow">
               <span>{val.toString()}</span>
             </div>
           )
